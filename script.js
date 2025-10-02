3cebec64e6b9dc75123743d5438e276c511e8136
ayaladk <ayalagustavo855@gmail.com>
2025-10-02 03:40:54 -0300

    Add simple functions
    - Adds basic operator functions (add, divide, multiply, substract)
    - Adds a main function that takes two numbers and a operator and call the basic functions

---
+++ script.js
@@ -0,0 +1,26 @@
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+function operate(number1, number2, operator) {
+	if (operator == '+') {
+		return add(number1, number2);
+	}
+	else if (operator == '-') {
+		return substract(number1, number2);
+	}
+	else if (operator == '*') {
+		return multiply(number1, number2);
+	}
+	else if (operator == '/') {
+		return divide(number1, number2);
+	}
+}
+
+
+let number1 = 1
+let number2 = 2
+let operator = '-'
+
+console.log(operate(number1, number2, operator))
